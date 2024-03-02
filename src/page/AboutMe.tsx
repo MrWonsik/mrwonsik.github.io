01565977c9f57053dead74b103b562a230a455c5
MrWonsik <mrt.wonsik@gmail.com>
2024-03-02 23:59:42 +0100

    Updates

--- src/page/AboutMe.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-//A brief introduction about yourself, including your name, professional title, and a summary of your skills, experiences, and interests.
-
-function AboutMe() {
-  return <div></div>;
-}
-
-export default AboutMe;
